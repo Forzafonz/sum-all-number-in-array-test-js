3c0b499b4167fd56d3f6867dad69c4a28a9a577c
Anton <golubin1990@gmail.com>
2021-07-25 18:53:55 -0400

    Intital commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  if (Array.isArray(array[0])) {
+    return sumItems(array[0]) + sumItems(array.splice(1));
+  } else if (array.length === 1) {
+    return array[0];
+  } else if (array.length === 0){
+    return 0;
+  } else {
+    return array[0] + sumItems(array.splice(1)); 
+  }
 }
 
 module.exports = sumItems;
